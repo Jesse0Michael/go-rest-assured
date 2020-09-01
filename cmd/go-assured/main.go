63f951025dab3a2982238dad6c8c1bdb2c0b0d64
Jesse Michael <jesse.michael1234@gmail.com>
2020-09-01 16:46:16 -0700

    feat: remove logger arg
    BREAKING CHANGE:
    
    remove logger option, if you want to redirect your logs in a file, use the appropriate cli commands

--- cmd/go-assured/main.go
+++ cmd/go-assured/main.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -30,60 +29,43 @@ func main() {
 	}()
 
 	port := flag.Int("port", 0, "a port to listen on. default automatically assigns a port.")
-	logFile := flag.String("logger", "", "a file to send logs. default logs to STDOUT.")
 	preload := flag.String("preload", "", "a file to parse preloaded calls from.")
 	trackMade := flag.Bool("track", true, "a flag to enable the storing of calls made to the service.")
 
 	flag.Parse()
 
-	// If logger specified, set the assured client to write to the file
-	if *logFile != "" {
-		file, err := os.Create(*logFile)
-		if err != nil {
-			logger.Log("fatal", err.Error())
-			os.Exit(1)
-		}
-		logger = kitlog.NewLogfmtLogger(file)
-	}
-
-	settings := assured.Settings{
-		Logger:         logger,
-		Port:           *port,
-		TrackMadeCalls: *trackMade,
-		HTTPClient:     *http.DefaultClient,
-	}
-	client := assured.NewClient(rootCtx, settings)
+	client := assured.NewClient(assured.WithContext(rootCtx), assured.WithPort(*port),
+		assured.WithCallTracking(*trackMade), assured.WithLogger(logger))
 
 	// If preload file specified, parse the file and load all calls into the assured client
 	if *preload != "" {
 		file, err := os.Open(*preload)
 		if err != nil {
-			logger.Log("fatal", err.Error())
+			_ = logger.Log("fatal", err.Error())
 			os.Exit(1)
 		}
 		b, err := ioutil.ReadAll(file)
 		if err != nil {
-			logger.Log("fatal", err.Error())
+			_ = logger.Log("fatal", err.Error())
 			os.Exit(1)
 		}
 		var preload Preload
 		// TODO response won't unmarshal string to []byte
 		if err := json.Unmarshal(b, &preload); err != nil {
-			logger.Log("fatal", err.Error())
+			_ = logger.Log("fatal", err.Error())
 			os.Exit(1)
 		}
-		client.Given(preload.Calls...)
-		if err != nil {
-			logger.Log("fatal", err.Error())
+		if err = client.Given(preload.Calls...); err != nil {
+			_ = logger.Log("fatal", err.Error())
 			os.Exit(1)
 		}
 	}
 
-	logger.Log("fatal", <-errc)
+	_ = logger.Log("fatal", <-errc)
 }
 
 func interrupt() error {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	return fmt.Errorf("%s", <-c)
 }
